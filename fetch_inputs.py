412f60d24b855654d786f1dddb95d9297437c241
helauren <helauren42@gmail.com>
2025-02-11 06:57:27 +0100

    fetched subjects too

--- fetch_inputs.py
+++ fetch_inputs.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 import subprocess
+from bs4 import BeautifulSoup
 
 URL = "https://adventofcode.com/"
 YEAR = int(sys.argv[1])
@@ -17,10 +18,21 @@ ID = getCookie().strip()
 COOKIE = {"session": ID}
 
 for i in range(1, 26):
-    response = requests.get(f"{URL}{YEAR}/day/{i}/input", cookies=COOKIE)
-    print(f"{URL}{YEAR}/day/{i}/input")
-    text = response.content.decode('utf-8')
     subprocess.run([f"mkdir -p Day{i}"], shell=True, cwd=PROJECT_DIR)
-    subprocess.run(["touch script.py input.txt"], shell=True, cwd=f"{PROJECT_DIR}Day{i}")
+    subprocess.run(["touch script.py subject.txt input.txt"], shell=True, cwd=f"{PROJECT_DIR}Day{i}")
+
+    # fetching subjects
+    response = requests.get(f"{URL}{YEAR}/day/{i}", cookies=COOKIE)
+    text = response.content.decode('utf-8')
+    soup = BeautifulSoup(text, "html.parser")
+    text = soup.get_text()
+    start = text.find("--- Day ")
+    end = text.find("To begin, get your puzzle input.")
+    with open(f"{PROJECT_DIR}Day{i}/subject.txt", "w") as file:
+        file.write(text)
+
+    # fetching input texts
+    response = requests.get(f"{URL}{YEAR}/day/{i}/input", cookies=COOKIE)
+    text = response.content.decode('utf-8')
     with open(f"{PROJECT_DIR}Day{i}/input.txt", "w") as file:
         file.write(text)
